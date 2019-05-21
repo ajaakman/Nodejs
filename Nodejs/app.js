2e4bc853c41f78811650c3f84fa2e47daadfff40
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-21 12:40:52 +0300

    Initial Commit.

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,3 +1,18 @@
-'use strict';
+var read = require('readline');
 
-console.log('Hello world');
+var a = 45;
+
+var main = function () {
+    console.log('Hello world');
+
+    read.createInterface(process.stdin, process.stdout)
+        .question("Press [e] to exit...", function (inputData) {
+            if (inputData === "e")
+                process.exit();
+            else {
+                main();
+            }
+        });
+}
+
+main();
