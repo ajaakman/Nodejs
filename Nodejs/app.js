1ac4ac39c460d8bfbc56639a950156773a2ea7fe
Artur Jaakman <jaakmanartur@gmail.com>
2019-05-21 14:11:33 +0300

    Eslint.

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,10 +1,11 @@
+'use strict'
+
 var read = require('readline');
 
-var a = 45;
 
 var main = function () {
     console.log('Hello world');
-
+        
     read.createInterface(process.stdin, process.stdout)
         .question("Press [e] to exit...", function (inputData) {
             if (inputData === "e")
